8ae68b3c8eb70f294f5339a9c5b93b7317b82ec3
Vetrox2 <mateuszmulka@spoko.pl>
2024-03-06 15:06:51 +0100

    Added bouncing mechanics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,7 +19,8 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     BoxCollider2D collider1;
     float jumpLoadingTime = 0;
-    private bool canJump = true;
+    private bool duringJump = false;
+    private bool waitAfterJump = false;
     private bool duringLoadingJump = false;
     float moveInput = 0;
 
@@ -32,19 +33,27 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = Input.GetAxis("Horizontal");
 
-        IsGrounded = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y-0.5f), new Vector2(0.2f, 0.4f), 0, GroundMask);
-        if (!duringLoadingJump && IsGrounded && canJump)
+        if (!duringLoadingJump && IsGrounded && !duringJump)
         {
             rb.velocity = new Vector2(MovementSpeed * moveInput, rb.velocity.y);
         }
-        //Debug.Log(IsGrounded);
-        if (collider1.sharedMaterial == PlayerJumpMaterial && canJump && IsGrounded)
-        {
-            rb.sharedMaterial = PlayerMaterial;
-        }
+        
         Debug.Log($"{IsGrounded}, {rb.velocity}         {Time.deltaTime}");
     }
- 
+    private void FixedUpdate()
+    {
+        if(!duringJump)
+            IsGrounded = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y-0.05f), collider1.size, 0, GroundMask);
+        else if (!waitAfterJump)
+            IsGrounded = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y-0.25f), collider1.size*0.8f, 0, GroundMask);
+        if (collider1.sharedMaterial == PlayerJumpMaterial && !waitAfterJump && IsGrounded)
+        {
+            rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+            collider1.sharedMaterial = PlayerMaterial;
+            duringJump = false;
+        }
+    }
+
     public void Jump(InputAction.CallbackContext context)
     {
         if (context.started && IsGrounded)
@@ -53,18 +62,19 @@ public class PlayerController : MonoBehaviour
             jumpLoadingTime = (float)context.startTime;
             rb.velocity = new Vector3(0, rb.velocity.y);
         }
-        else if (context.canceled && IsGrounded && canJump && duringLoadingJump)
+        else if (context.canceled && IsGrounded && !duringJump && duringLoadingJump)
         {
             collider1.sharedMaterial = PlayerJumpMaterial;
+            rb.interpolation = RigidbodyInterpolation2D.Extrapolate;
             jumpLoadingTime = (float)context.startTime - jumpLoadingTime;
             duringLoadingJump = false;
-            canJump = false;
+            duringJump = true;
+            waitAfterJump = true;
             float jumpX = moveInput > 0 ? 1 : -1;
             jumpX = moveInput == 0 ? 0 : jumpX;
             rb.velocity = new Vector2(MovementSpeed * jumpX/1.2f, Mathf.Lerp(MinJumpHeight, MaxJumpHeight, jumpLoadingTime / JumpMaxLoadingTime));
             Invoke("JumpCD", 0.2f);
         }
     }
-    void JumpCD() => canJump = true;
-
+    void JumpCD() => waitAfterJump = false;
 }
