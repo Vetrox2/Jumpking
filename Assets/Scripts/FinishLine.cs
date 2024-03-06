5143e4c2382a3a55858e84176f588f0e8a709e66
Vetrox2 <mateuszmulka@spoko.pl>
2024-03-06 19:29:05 +0100

    Added finishLine

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    public GameController controller;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>() != null)
+            controller.Finished();
+    }
+}
