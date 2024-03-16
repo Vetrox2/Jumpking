55b6d59951e11d57447c8431d986f0446a5154f9
Vetrox2 <mateuszmulka@spoko.pl>
2024-03-17 00:20:44 +0100

    New highscore table

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,22 +2,28 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     [SerializeField]
-    GameObject MainMenu;
-    [SerializeField]
-    GameObject RankingMenu;
-    [SerializeField]
-    GameObject SetNameScreen;
-    [SerializeField]
-    TMP_InputField SetNameField;
+    GameObject MainMenu, RankingMenu;
     [SerializeField]
     ScoreTable[] scoreTable;
+    [SerializeField]
+    Button WorldHighscoreButton, UserHighscoreButton;
 
-    GameObject currentMenu;
+    private GameObject currentMenu;
+    private List<Highscore> userHighscores, worldHighscores;
+    private HighscoreType activeHighscoreType;
 
+    private void Start()
+    {
+        activeHighscoreType = HighscoreType.WorldHighscore;
+    }
     public void NewGame()
     {
         SaveLoad.DeleteSave();
@@ -36,13 +42,62 @@ public class Menu : MonoBehaviour
         realmController.RealmLoaded += GetHighscore;
         realmController.InitAsync();
     }
+
+    public void ChangeRankingView(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                activeHighscoreType = HighscoreType.WorldHighscore;
+                break; 
+            case 1:
+                activeHighscoreType = HighscoreType.UserHighscore;
+                break;
+        }
+        UpdateRanking();
+    }
+    
     void GetHighscore(RealmController<Highscore> realmController)
     {
         var scores = realmController.GetHighscore();
-        for (int i = 0; i < scoreTable.Length && i < scores.Count; i++)
+        worldHighscores = scores.GroupBy(score => score.Player)
+                        .Select(group => new Highscore
+                        {
+                            Player = group.Key,
+                            Time = group.Min(score => score.Time)
+                        }).OrderBy(player => player.Time).Take(scoreTable.Length).ToList();
+
+        userHighscores = scores.Where(score => score.Player == PlayerPrefs.GetString("UserName"))
+                        .OrderBy(player => player.Time).Take(scoreTable.Length).ToList();
+        UpdateRanking();
+    }
+    void UpdateRanking()
+    {
+        for (int i = 0; i < scoreTable.Length; i++)
         {
-            scoreTable[i].name.text = scores[i].Player;
-            scoreTable[i].time.text = scores[i].Time.ToString();
+            scoreTable[i].name.text = string.Empty;
+            scoreTable[i].time.text = string.Empty;
+        }
+        switch (activeHighscoreType)
+        {
+            case HighscoreType.WorldHighscore:
+                UserHighscoreButton.interactable = true;
+                WorldHighscoreButton.interactable = false;
+                for (int i = 0; i < scoreTable.Length && i < worldHighscores.Count; i++)
+                {
+                    scoreTable[i].name.text = worldHighscores[i].Player;
+                    scoreTable[i].time.text = worldHighscores[i].Time.ToString();
+                }
+                break;
+            case HighscoreType.UserHighscore:
+                UserHighscoreButton.interactable = false;
+                WorldHighscoreButton.interactable = true;
+                for (int i = 0; i < scoreTable.Length && i < userHighscores.Count; i++)
+                {
+                    scoreTable[i].name.text = userHighscores[i].Player;
+                    scoreTable[i].time.text = userHighscores[i].Time.ToString();
+                }
+                break;
         }
     }
     public void Escape(InputAction.CallbackContext contex)
@@ -56,4 +111,9 @@ public class Menu : MonoBehaviour
         currentMenu.SetActive(false);
     }
 
+}
+public enum HighscoreType
+{
+    WorldHighscore,
+    UserHighscore
 }
